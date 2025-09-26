e643121e5f9f05529fc554ca24393888f5c0840b
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-09-26 12:30:44 +0800

    Add JS for multiple dropdown menus

--- src/modules/dropdownMenu.js
+++ src/modules/dropdownMenu.js
@@ -1,13 +1,17 @@
 export function dropdownMenu() {
-  const dropdownMenu = document.querySelector(".has-secondary-list");
+  const dropdownMenus = document.querySelectorAll(".has-secondary-list");
 
-  dropdownMenu.addEventListener("mouseenter", (e) => {
-    const secondaryList = e.target.querySelector(".secondary-list");
-    secondaryList.classList.add("shown");
+  dropdownMenus.forEach((menu) => {
+    menu.addEventListener("mouseenter", (e) => {
+      const secondaryList = e.target.querySelector(".secondary-list");
+      secondaryList.classList.add("shown");
+    });
   });
 
-  dropdownMenu.addEventListener("mouseleave", (e) => {
-    const secondaryList = e.target.querySelector(".secondary-list");
-    secondaryList.classList.remove("shown");
+  dropdownMenus.forEach((menu) => {
+    menu.addEventListener("mouseleave", (e) => {
+      const secondaryList = e.target.querySelector(".secondary-list");
+      secondaryList.classList.remove("shown");
+    });
   });
 }
